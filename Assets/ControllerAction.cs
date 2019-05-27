682a7b543699a1d2016446b4a8ce8042a8954e38
o0kazuya0o <o0kazuya0o@ezweb.ne.jp>
2019-05-28 00:44:55 +0900

    SampleScene編集

---
+++ Assets/ControllerAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ControllerAction : MonoBehaviour
+{
+    public SteamVR_Input_Sources hand;
+    public SteamVR_Action_Boolean grabAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (grabAction.GetState(hand))
+        {
+            Debug.Log(hand.ToString() + "Grab!");
+        }
+    }
+}
