11b8b7c0eefedcfddcbac7f90a03936150707b6e
o0kazuya0o <o0kazuya0o@ezweb.ne.jp>
2019-06-04 14:15:24 +0900

    敵を倒した時のエフェクトを追加

---
+++ Assets/archery/Script/EffectDelete.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//パーティクルシステムコンポーネントがない場合は自動追加
+[RequireComponent(typeof(ParticleSystem))]
+
+public class EffectDelete : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParticleSystem partcleSystem = GetComponent<ParticleSystem>();
+        //Delete object after duration.
+        Destroy(gameObject, (float)partcleSystem.main.duration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
